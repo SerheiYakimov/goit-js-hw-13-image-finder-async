db80ed73f6e3d39f3072866e1946677efe28c9e2
SerheiYakimov <ysergey2901@gmail.com>
2021-09-12 14:30:22 +0300

    add class

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import { alert } from '../node_modules/@pnotify/core/dist/PNotify.js';
 import '@pnotify/core/dist/BrightTheme.css';
 import * as basicLightbox from 'basiclightbox';
 import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
-import {Pagination} from 'tui-pagination';
+import { Pagination } from 'tui-pagination';
+// import { Pagination } from './js/pagination.js';
 import './sass/main.scss';
 
 
@@ -23,14 +24,54 @@ refs.form.addEventListener('submit', onFetchImages);
 refs.moreBtn.addEventListener('click', onLoadMore);
 refs.upBtn.addEventListener('click', onScrollUp);
 
+// myPagination.on('afterMove', (event) => {
+//         const currentPage = event.page;
+//         console.log(currentPage);
+//     }
+//     );
+
+// myPagination.on('beforeMove', (event) => {
+//         const currentPage = event.page;
+
+//         if (currentPage === 10) {
+//             return false;
+//             // return true;
+//         }
+//     });
+
 
 
    
 const imageApiService = new ImageApiService();
 const hiddenElement = document.getElementById('more');
+const container = document.getElementById('tui-pagination-container');
+const options = { // below default value of options
+    totalItems: 10,
+    itemsPerPage: 10,
+    visiblePages: 10,
+    page: 1,
+    centerAlign: false,
+    firstItemClassName: 'tui-first-child',
+    lastItemClassName: 'tui-last-child',
+    template: {
+        page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+        currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+        moveButton:
+            '<a href="#" class="tui-page-btn tui-{{type}}">' +
+            '<span class="tui-ico-{{type}}">{{type}}</span>' +
+            '</a>',
+        disabledMoveButton:
+            '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+            '<span class="tui-ico-{{type}}">{{type}}</span>' +
+            '</span>',
+        moreButton:
+            '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+            '<span class="tui-ico-ellip">...</span>' +
+            '</a>'
+    }
+};
 
-
-
+const myPagination = new Pagination(container, options);
 
 
 function onFetchImages(e) {
@@ -115,37 +156,44 @@ function onShowFullImage(e) {
 
 
 function ShowPagination() {
-    const Pagination = tui.Pagination;
+    // const Pagination = tui.Pagination;
+    // const myPagination = new Pagination(container, {
+    //         totalItems: 500,
+    //         itemsPerPage: 10,
+    //         visiblePages: 5,
+    //         centerAlign: true
+    // });
 
-    const container = document.getElementById('pagination');
-    const options = { // below default value of options
-        totalItems: 10,
-        itemsPerPage: 10,
-        visiblePages: 10,
-        page: 1,
-        centerAlign: false,
-        firstItemClassName: 'tui-first-child',
-        lastItemClassName: 'tui-last-child',
-        template: {
-            page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-            currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-            moveButton:
-                '<a href="#" class="tui-page-btn tui-{{type}}">' +
-                    '<span class="tui-ico-{{type}}">{{type}}</span>' +
-                '</a>',
-            disabledMoveButton:
-                '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-                    '<span class="tui-ico-{{type}}">{{type}}</span>' +
-                '</span>',
-            moreButton:
-                '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-                    '<span class="tui-ico-ellip">...</span>' +
-                '</a>'
-     }
+    
+
+    
+
+
+
+    
+   
+
+        // gets the current page
+    myPagination.getCurrentPage();
+
+    // goes to page x
+    myPagination.movePageTo(targetPage);
+
+    // resets the pagination
+    myPagination.reset(totalItems);
+
+    // sets the number of items per page
+    myPagination.gsetItemsPerPage(itemCount);
+
+    // sets the total number of items
+    myPagination.setTotalItems(itemCount);
+
+
+
+
 };
-const pagination = new Pagination(container, options);
 
-});
+
 
 
 
